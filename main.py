67f2b14dc4997a20e82cfc0924ef694588714369
e-landeros <landerosedgard@gmail.com>
2023-04-26 20:43:40 -0700

    First commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from typing import Optional, List
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+users = []
+
+
+class User(BaseModel):
+    email: str
+    is_active: bool
+    bio: Optional[str] = None
+
+
+@app.get("/users", response_model=List[User])
+async def get_users():
+    return users
+
+
+@app.post("/users")
+async def create_user(user: User):
+    users.append(user)
+    return {"message": f"User {user} has been created!"}
