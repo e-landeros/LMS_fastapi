c81a1721d84401bd894896c426c5eb3825a6a4bd
e-landeros <landerosedgard@gmail.com>
2023-04-29 10:45:03 -0700

    update to base

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from typing import Optional, List
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, Query
 from pydantic import BaseModel
 
-app = FastAPI()
+app = FastAPI(
+    title="FastAPI LMS",
+    description="lms for managing students and courses",
+    version="0.0.1",
+    contact={
+        "name": "Fabian Landeros",
+        "email": "landerosedgard@gmail.com"
+    },
+    license_info={
+        "name": "MIT",
+        "url": "https://opensource.org/licenses/MIT"
+    },
+
+)
 
 users = []
 
@@ -26,8 +39,7 @@ async def create_user(user: User):
 
 @app.get("/users/{id}")
 async def get_user(
-        id: int = Path(..., description="The ID of the user you want to retrieve.", gt=2)
+        id: int = Path(..., description="The ID of the user you want to retrieve.", gt=2),
+        q: str = Query(None,max_length=5)
 ):
-    return users[id]
-
-# min 36:00
+    return {"user": users[id], "query": q}
